48a0dd475b932a6a5d61617e9daad9e3b01e6c7c
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-11-22 22:21:32 +0000

    set up repro example

--- packages/package-a/src/index.ts
+++ packages/package-a/src/index.ts
@@ -1,50 +1,11 @@
 /**
- * Test package
+ * Foo
  *
- * @remarks
- *
- * This remarks block includes a bulleted list!
- *
- * - Bullet 1
- *
- * - Bullet 2
- *
- * And an ordered list for good measure!
- *
- * 1. List item 1
- *
- * 2. List item 2
- *
- * 3. List item 3
- *
- * Also, here is a link test, including a bad link, because we should have some reasonable support if this happens:
- *
- * - Good link (no alias): {@link TestClass}
- *
- * - Good link (with alias): {@link testFunction | function alias text}
- *
- * - Bad link (no alias): {@link InvalidItem}
- *
- * - Bad link (with alias):
- *   {@link InvalidItem | even though I link to an invalid item, I would still like this text to be rendered}
- *
- * @example
- *
- *A test example
- *```typescript
- *const foo = bar;
- *```
- *
- * @packageDocumentation
+ * @internal
  */
-
-import * as TestModule from './TestModule';
-
-export * from './TestClass';
-export * from './TestConst';
-export * from './TestEnum';
-export * from './TestFunction';
-export * from './TestInterface';
-export { TestModule };
-export * from './TestNamespace';
-export * from './TestType';
+export interface Foo {
+	/**
+	 * x
+	 */
+	x: string;
+}
--- packages/package-b/src/index.ts
+++ packages/package-b/src/index.ts
@@ -1,7 +1,13 @@
-/**
- * Another test package
- *
- * @packageDocumentation
- */
+import { Foo } from 'package-a';
 
-export { testFunction } from 'package-a';
+/**
+ * Bar
+ *
+ * @public
+ */
+export class Bar implements Foo {
+	/**
+	 * {@inheritDoc package-a#Foo.x}
+	 */
+	x = 'x';
+}
